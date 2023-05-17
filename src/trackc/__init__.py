b5cac9a70303e5af4cedf696652332410775ffd2
seqyuan <ahworld07@gmail.com>
2023-05-17 22:58:51 +0800

    v0.1.7

--- src/trackc/__init__.py
+++ src/trackc/__init__.py
@@ -1,7 +1,5 @@
 # the actual API
-from trackc import tools as tl
-from trackc import plotting as pl
-from trackc import palettes as pa
+from trackc tl, pl, pa
 from trackc.gs import (
     make_spec,
     tenon,
@@ -16,10 +14,8 @@ __all__ = [
     "tenon"
 ]
 
-"""
 import sys
 sys.modules.update({f'{__name__}.{m}': globals()[m] for m in ['tl', 'pl', 'pa']})
 from ._utils import annotate_doc_types
 annotate_doc_types(sys.modules[__name__], 'trackc')
 del sys, annotate_doc_types
-"""
