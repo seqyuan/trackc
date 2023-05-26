205feb58a7f2b002633a5091e8394499f85fdaa5
seqyuan <ahworld07@gmail.com>
2023-05-26 18:07:35 +0800

    0.0.7

--- setup.py
+++ setup.py
@@ -26,9 +26,9 @@ setuptools.setup(
     #package_dir={'trackc': 'src/trackc'},
     #package_data={'trackc': ['qc_template.html']},
     url='http://trackc.readthedocs.io',
-            
     description="Track view of chromosome conformation and multi-omics data",
     #long_description=open('README.rst').read(),
+    long_description='xx',
     #url="https://github.com/seqyuan/trackc",
     classifiers=[
         "Programming Language :: Python :: 3",
