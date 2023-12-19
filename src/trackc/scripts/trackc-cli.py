86b512ec9e17dfc03e1235787dfe61790d0c4e5e
seqyuan <ahworld07@gmail.com>
2023-12-19 17:21:35 +0800

    pytest

--- src/trackc/scripts/trackc-cli.py
+++ src/trackc/scripts/trackc-cli.py
@@ -1,10 +1,16 @@
 import click
 
 @click.command()
-@click.option('--regions', '-r', required=True, 
+@click.argument('--regions', '-r', required=True, 
               help="genome regions, eg.: 18:47950000-48280000 18:75280000-74850000")
-@click.option('--outfile', '-o', default='trackc.pdf',
+@click.argument('--outfile', '-o', default='trackc.pdf',
               help='output filename')
+
+@click.option('--baseFigsize', '-s', default='8,1',
+              help='base figsize: width,height . \
+              The height option in the config.yml is relative to the base figsize height')
+
+
 def hello(regions, outfile):
     print(1111, regions)
     print(1111, outfile)
